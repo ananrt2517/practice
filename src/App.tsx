4fd3f18cba2d65facfc8a4d0bba700a3911b0383
ananrt2517 <stepanovicana6@gmail.com>
2021-04-20 11:29:09 +0200

    complete react routering and adding header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import Header from './components/Header/Header';
+import Home from './pages/Home/Home';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      ana
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/home" component={ Home }/>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React, { ReactElement } from 'react';
+import { HeaderDiv, Logo, Link } from './HeaderStyles'
+
+interface Props {
+  
+}
+
+export default function Header({}: Props): ReactElement {
+  return (
+    <HeaderDiv>
+      <Logo><Link href="/home">LOGO</Link></Logo>
+    </HeaderDiv>
+  )
+}
